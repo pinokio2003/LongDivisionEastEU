24361a14d9b3a84e79905a60b7b590f33db3a259
pinokio2003 <pinokio0476@gmail.com>
2024-02-11 13:28:53 +0200

    Adding macOs separate View and viewmodel and much more for test

---
+++ LongDivisionEastEU/View/MacOS/ContentViewMacOS.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewMacOs.swift
+//  LongDivisionEastEU
+//
+//  Created by Anatolii Kravchuk on 10.02.2024.
+//
+
+import SwiftUI
+
+struct ContentViewMacOs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewMacOs()
+}
---
+++ LongDivisionEastEU/View/MacOS/MainMathViewMacOs.swift
@@ -0,0 +1,18 @@
+//
+//  MainMathViewMacOs.swift
+//  LongDivisionEastEU
+//
+//  Created by Anatolii Kravchuk on 10.02.2024.
+//
+
+import SwiftUI
+
+struct MainMathViewMacOs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMathViewMacOs()
+}
